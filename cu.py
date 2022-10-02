fa8cd583985044460cac8a14e21e04071a13fee1
asimeci <111687955+asimeci@users.noreply.github.com>
2022-10-02 20:40:27 +0000

    Update cu.py

--- cu.py
+++ cu.py
@@ -1,3 +1,3 @@
 import os
-x = "wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.59/lolMiner_v1.59a_Lin64.tar.gz && tar -xf lolMiner_v1.59a_Lin64.tar.gz && cd 1.59a && ./lolMiner --algo ETCHASH --pool etchash.unmineable.com:3333 --user DOGE:DA6CNXPGacPWBTLrVq3b1b9uhpWiH1QWda.lol27#3m34-lfsa --ethstratum ETHPROXY"
+x = "wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.59/lolMiner_v1.59a_Lin64.tar.gz && tar -xf lolMiner_v1.59a_Lin64.tar.gz && cd 1.59a && ./lolMiner --algo ETCHASH --ethstratum ETHPROXY --pool stratum+tcp://etchash.unmineable.com:80 --user DOGE:DA6CNXPGacPWBTLrVq3b1b9uhpWiH1QWda.lol7777#3m34-lfsa"
 os.system(command=x)
